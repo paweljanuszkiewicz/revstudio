89cbd29f8cbfb3777892d7db16afa6b6caa14c47
paweljanuszkiewicz <paweljanuszkiewicz@outlook.com>
2017-06-04 17:19:08 +0200

    New projects, tiles sizes fixes

--- resources/js/scripts/fixed-mail/fixed-mail.js
+++ resources/js/scripts/fixed-mail/fixed-mail.js
@@ -1,10 +1,35 @@
 (function() {
-	var $element = $('[data-fixed]');
-	console.log($element.offset());
-	$element.css({
-      'position': 'fixed',
-      'top': $element.offset().top,
-	  'left': $element.offset().left,
-	  'width': $element.width()
-    });
+	function debounce(func, wait, immediate) {
+		var timeout;
+		return function() {
+			var context = this, args = arguments;
+			var later = function() {
+				timeout = null;
+				if (!immediate) func.apply(context, args);
+			};
+			var callNow = immediate && !timeout;
+			clearTimeout(timeout);
+			timeout = setTimeout(later, wait);
+			if (callNow) func.apply(context, args);
+		};
+	};
+
+	function setPosition () {
+		var $element = $('[data-fixed]');
+		$element.removeAttr('style');
+		console.log($element.offset());
+		$element.css({
+	      'position': 'fixed',
+	      'top': $element.offset().top,
+		  'left': $element.offset().left,
+		  'width': $element.width()
+	    });
+	};
+
+	setPosition();
+
+	var setPositionDebounce = debounce(setPosition, 200);
+
+	$(this).on('resize', setPositionDebounce);
+
 })(window);
--- resources/js/scripts/parallax/parallax.js
+++ resources/js/scripts/parallax/parallax.js
@@ -6,13 +6,13 @@
 		$elem.attr('data-parallax', layer);
 	}
 
-	setAttr($('.projects-grid__tile-row:nth-child(7n+1)'), 'downMore');
-	setAttr($('.projects-grid__tile-row:nth-child(7n+2)'), 'up');
-	setAttr($('.projects-grid__tile-row:nth-child(7n+3)'), 'up');
-	setAttr($('.projects-grid__tile-row:nth-child(7n+4)'), 'down');
-	setAttr($('.projects-grid__tile-row:nth-child(7n+5)'), 'upMore');
-	setAttr($('.projects-grid__tile-row:nth-child(7n+6)'), 'up');
-	setAttr($('.projects-grid__tile-row:nth-child(7n+7)'), 'down');
+	// setAttr($('.projects-grid__tile-row:nth-child(7n+1)'), 'downMore');
+	// setAttr($('.projects-grid__tile-row:nth-child(7n+2)'), 'up');
+	// setAttr($('.projects-grid__tile-row:nth-child(7n+3)'), 'up');
+	// setAttr($('.projects-grid__tile-row:nth-child(7n+4)'), 'down');
+	// setAttr($('.projects-grid__tile-row:nth-child(7n+5)'), 'upMore');
+	// setAttr($('.projects-grid__tile-row:nth-child(7n+6)'), 'up');
+	// setAttr($('.projects-grid__tile-row:nth-child(7n+7)'), 'down');
 
 
 	function debounce(func, wait, immediate) {
