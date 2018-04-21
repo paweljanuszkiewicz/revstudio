521579404ef9cb5df486d04568720c13595cf73c
paweljanuszkiewicz <paweljanuszkiewicz@outlook.com>
2018-04-21 17:38:39 +0200

    Add changes

--- resources/js/scripts/mobile-menu/mobile-menu.js
+++ resources/js/scripts/mobile-menu/mobile-menu.js
@@ -3,6 +3,7 @@
 	var $menu = $('[data-mobile-menu="menu"]');
 
 	$hamburger.on('click', function () {
+		$(this).closest('.header').toggleClass('is-active');
 		$(this).toggleClass('is-active');
 		$menu.toggleClass('is-active');
 	});
